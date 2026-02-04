605d6961fa59be150de2433c466dd4126feb7208
Syed Jawwad Kareem Naqvi <139786195+jawwadkareem@users.noreply.github.com>
2026-02-04 13:26:18 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -66,8 +66,8 @@ function App() {
 
     const utter = new SpeechSynthesisUtterance(text);
     if (selectedVoiceRef.current) utter.voice = selectedVoiceRef.current;
-    utter.rate  = 1.05;
-    utter.pitch = 1.0;
+    utter.rate  = 1.35;
+    utter.pitch = 0.9;
 
     utter.onend = () => {
       isSpeakingRef.current = false;
